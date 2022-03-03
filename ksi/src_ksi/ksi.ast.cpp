c28ff205f0edf4e079aeeec8d69fd7acefd32d9b
deemetrius <sm0ke999@yandex.ru>
2022-03-03 13:16:58 +0300

    upd prec

--- ksi/src_ksi/ksi.ast.cpp
+++ ksi/src_ksi/ksi.ast.cpp
@@ -7,8 +7,11 @@ export namespace ksi::ast {
 // precedence
 enum n_prec {
 	prec_leaf,
+	//
+	prec_top_before,
 	prec_sign_before		//	-a +a
-	= prec_leaf,
+	= prec_top_before,
+	//
 	prec_top_after,
 	prec_type_of_after		//	a$ a& a&&		(type of, get ref, get weak ref)
 	= prec_top_after,
@@ -16,12 +19,14 @@ enum n_prec {
 	= prec_top_after,
 	prec_not_after			//	`not			(logical, unary postfix)
 	= prec_top_after,
+	//
 	prec_member,			//	a.key a[key]
 	prec_sign_after,		//	-a +a
 	prec_type_of_before,	//	a$ a& a&&		(type of, get ref, get weak ref)
 	prec_rt_assign_after,	//	=>
 	prec_assign_before,		//	=
 	prec_invoke,			//	fn! & &&		(fn call by ref)
+	prec_pow,				//	**
 	prec_mult,				//	* / `mod
 	prec_and_bits,			//	&				(bitwise)
 	prec_xor_bits,			//	^
@@ -42,6 +47,7 @@ enum n_prec {
 	prec_or,				//	`or
 	prec_not_before,		//	`not			(logical, unary postfix)
 	prec_pair,				//	:				(key-value pair)
+	//
 	prec_root
 };
 
